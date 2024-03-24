16361e8d15a8c80030eeec17a1960ee20bf9e596
prince2998 <princegupta2971998@gmail.com>
2024-03-24 15:21:42 +0530

    update

--- ansible_config/k8s/replace_k8s_ip.sh
+++ ansible_config/k8s/replace_k8s_ip.sh
@@ -1,22 +1,28 @@
 ### This script replaces the existing ip address of the k8s-master and k8s-node1 with the dummy values which are required for the add_k8s_ip.sh script.
 #!/bin/bash
 
-mip=`grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' master.sh | head -n 1` 
-n1ip=`grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' master.sh | head -n 2`
-n2ip=`grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' master.sh | head -n 3`
+echo "Fetching Existing IPs of k8s-master, k8s-node1, k8-node2 to the master.sh and nodes.sh scripts"
+mip=`grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' ../../../master.sh | head -n 1`
+n1ip=$(grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' ../../../master.sh | head -n 2 | awk 'NR==2')
+n2ip=$(grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' ../../../master.sh | head -n 3 | sed -n '3p')
 
-if df | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' master.sh;
-then 
+echo "Replacing IPs of k8s-master, k8s-node1, k8-node2 to the master.sh and nodes.sh scripts"
+new_mip=`terraform -chdir="../../terraform_config/master" output | grep private_ip |awk '{print $3}' | tr -d '"'`
+new_n1ip=`terraform -chdir="../../terraform_config/node1" output | grep private_ip |awk '{print $3}' | tr -d '"'`
+new_n2ip=`terraform -chdir="../../terraform_config/node2" output | grep private_ip |awk '{print $3}' | tr -d '"'`
+
+if df | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' ../ansible_config/k8s/master.sh;
+then
 {
 echo "Previously used IPs found"
-echo "Replacing previously used IPs of k8s-master and k8s-node1 with dummy variables kmaster-ip, knode1-ip, knod2-ip in master.sh and nodes.sh scripts"
-sed -i "s/$mip/kmaster-ip/g" master.sh
-sed -i "s/$n1ip/knode1-ip/g" master.sh
-sed -i "s/$n2ip/knode2-ip/g" master.sh
+#echo "Replaced previously used IPs of k8s-master, k8s-node1, k8s-node2 with dummy variables kmaster-ip, knode1-ip, knode2-ip in master.sh and nodes.sh scripts"
+sed -i "s/$mip/$new_mip/g" ../../../master.sh
+sed -i "s/$n1ip/$new_n1ip/g" ../../../master.sh
+sed -i "s/$n2ip/$new_n2ip/g" ../../../master.sh
 
-sed -i "s/$mip/kmaster-ip/g" nodes.sh
-sed -i "s/$n1ip/knode1-ip/g" nodes.sh
-sed -i "s/$n2ip/knode2-ip/g" nodes.sh
+sed -i "s/$mip/$new_mip/g" ../../../nodes.sh
+sed -i "s/$n1ip/$new_n1ip/g" ../../../nodes.sh
+sed -i "s/$n2ip/$new_n2ip/g" ../../../nodes.sh
 }
 else echo "Dummy variables already present";
 fi
