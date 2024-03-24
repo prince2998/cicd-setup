7921b07b61903de36cdc9bfb21409ed84c080394
prince2998 <princegupta2971998@gmail.com>
2024-03-24 09:19:07 +0530

    update

--- ansible_config/k8s/replace_k8s_ip.sh
+++ ansible_config/k8s/replace_k8s_ip.sh
@@ -2,7 +2,8 @@
 #!/bin/bash
 
 mip=`grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' master.sh | head -n 1` 
-nip=`grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' master.sh | tail -n 1`
+n1ip=`grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' master.sh | head -n 2`
+n2ip=`grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' master.sh | head -n 3`
 
 if df | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' master.sh;
 then 
@@ -10,12 +11,12 @@ then
 echo "Previously used IPs found"
 echo "Replacing previously used IPs of k8s-master and k8s-node1 with dummy variables kmaster-ip, knode1-ip, knod2-ip in master.sh and nodes.sh scripts"
 sed -i "s/$mip/kmaster-ip/g" master.sh
-sed -i "s/$nip/knode1-ip/g" master.sh
-sed -i "s/$nip/knode2-ip/g" master.sh
+sed -i "s/$n1ip/knode1-ip/g" master.sh
+sed -i "s/$n2ip/knode2-ip/g" master.sh
 
 sed -i "s/$mip/kmaster-ip/g" nodes.sh
-sed -i "s/$nip/knode1-ip/g" nodes.sh
-sed -i "s/$nip/knode2-ip/g" nodes.sh
+sed -i "s/$n1ip/knode1-ip/g" nodes.sh
+sed -i "s/$n2ip/knode2-ip/g" nodes.sh
 }
 else echo "Dummy variables already present";
 fi
