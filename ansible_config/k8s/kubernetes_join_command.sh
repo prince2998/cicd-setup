2181c3113edce6f0e77368d81caf0348a429d754
prince2998 <princegupta2971998@gmail.com>
2024-03-28 09:45:59 +0530

    update

--- ansible_config/k8s/kubernetes_join_command.sh
+++ ansible_config/k8s/kubernetes_join_command.sh
@@ -1,1 +1,1 @@
-kubeadm join 172.31.42.153:6443 --token 7hnvhw.jh6mi439wg6gai2a --discovery-token-ca-cert-hash sha256:b78fe1a267c9ea96fd417f4f209a07722bddc88f92530cb230d9840c9d99885f 
+kubeadm join 172.31.42.153:6443 --token vvo41o.9tnicedqj33k8jeq --discovery-token-ca-cert-hash sha256:b78fe1a267c9ea96fd417f4f209a07722bddc88f92530cb230d9840c9d99885f 
